ac8bfb3531ae09b69808ca5f98df9f414f582f3a
Kosuke Abe <kousuke1201abe@gmail.com>
2019-03-20 13:28:15 +0900

    add a association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :name, { presence: true, uniqueness: true }
   validates :password, { presence: true, length:{ minimum: 8 } }
   has_many :posts, dependent: :destroy
+  has_many :answers, dependent: :destroy
   has_one :keyword, dependent: :destroy
   has_one :artist, dependent: :destroy
 end
